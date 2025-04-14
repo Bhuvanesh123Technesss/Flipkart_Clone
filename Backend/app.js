791e9e3665b6bc2ebfa6c8b95dcdebfe66de312c
Bhuvanesh A <bhuvaneshtechnesssolutions@gmail.com>
2025-04-14 13:53:10 +0530

    included dist on app.js

--- Backend/app.js
+++ Backend/app.js
@@ -27,6 +27,14 @@ app.listen(process.env.PORT, () => {
 
 //app.get("/", (req, res) => res.send("Express on Vercel"));
 
+
+if(process.env.NODE_ENV === 'PRODUCTION') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'))
+    })
+}
+
 app.get('/test', (req, res) => {
     res.send('Server is working!');
 });
